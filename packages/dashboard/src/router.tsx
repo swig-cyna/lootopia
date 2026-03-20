8df7edae691a6a0c5866f036e10832e30fae5116
dissemblable <julesvallet23@gmail.com>
2026-03-20 15:58:55 +0100

    feat(hunt): add new route for hunt

--- packages/dashboard/src/router.tsx
+++ packages/dashboard/src/router.tsx
@@ -1,7 +1,8 @@
 import ProtectedRoute from "@lootopia/dashboard/features/auth/components/ProtectedRoute"
-import DashboardLayout from "@lootopia/dashboard/pages/(dashboard)/layout"
 import IndexPage from "@lootopia/dashboard/pages/(dashboard)/index"
+import DashboardLayout from "@lootopia/dashboard/pages/(dashboard)/layout"
 import ErrorPage from "@lootopia/dashboard/pages/error"
+import HuntPage from "@lootopia/dashboard/pages/hunt"
 import NotFoundPage from "@lootopia/dashboard/pages/not-found"
 import SigninPage from "@lootopia/dashboard/pages/signin"
 import { createBrowserRouter, RouterProvider } from "react-router"
@@ -19,6 +20,10 @@ const router = createBrowserRouter([
               {
                 path: "/",
                 element: <IndexPage />,
+              },
+              {
+                path: "/hunt",
+                element: <HuntPage />,
               },
             ],
           },
